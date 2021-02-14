1acc4d539c46cc0d2c49a07549e81728adc2b9fc
Patryk Lizon <lizon.patryk@gmail.com>
2021-02-14 13:25:29 +0100

    refactor: Use test instead of suite

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -5,34 +5,34 @@ import { flatten } from "./example";
 
 test("returns unmodified flat array", () => {
   const array = [1, 2, 3, 4, 5, 6, 7];
-  const given = flatten(array);
+  const actual = flatten(array);
   const expected = [...array];
 
-  assert.equal(given, expected);
+  assert.equal(actual, expected);
 });
 
 test("flattens deeply nested array", () => {
   const array = [1, [2, [3, [4, [5, [6, [7]]]]]]];
-  const given = flatten(array);
+  const actual = flatten(array);
   const expected = [1, 2, 3, 4, 5, 6, 7];
 
-  assert.equal(given, expected);
+  assert.equal(actual, expected);
 });
 
 test("flattens multiple arrays on the same nest level", () => {
   const array = [1, [2, 3], [4, 5], 6, 7];
-  const given = flatten(array);
+  const actual = flatten(array);
   const expected = [1, 2, 3, 4, 5, 6, 7];
 
-  assert.equal(given, expected);
+  assert.equal(actual, expected);
 });
 
 test("flattens array with multiple nested levels", () => {
   const array = [1, [2, [3, 4], 5], 6, [7]];
-  const given = flatten(array);
+  const actual = flatten(array);
   const expected = [1, 2, 3, 4, 5, 6, 7];
 
-  assert.equal(given, expected);
+  assert.equal(actual, expected);
 });
 
 test.run();
