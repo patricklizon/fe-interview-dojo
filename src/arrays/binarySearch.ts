3d9499240b9639cd449b5e61884cbaef081eb77e
Patryk Lizon <lizon.patryk@gmail.com>
2021-02-16 00:12:16 +0100

    refactor: simplify binary search

--- src/arrays/binarySearch.ts
+++ src/arrays/binarySearch.ts
@@ -1,4 +1,4 @@
-// 1. Let min = 1 and max = n.
+// 1. Let min = 0 and max = n.
 // 2. Guess the average of max and min, rounded down so that it is an integer.
 // 3. If you guessed the number, stop. You found it!
 // 4. If the guess was too low, set min to be one larger than the guess.
@@ -9,17 +9,14 @@ export const binarySearch = (xs: number[], num: number): number => {
   let min = 0;
   let max = xs.length;
 
-  while (min <= max) {
-    const average = Math.floor((min + max) / 2);
-    const value = xs[average] as number;
+  while (min < max) {
+    const idx = Math.floor((min + max) / 2);
+    const value = xs[idx];
 
-    if (value === num) return average;
+    if (value === num) return idx;
+    if (!value) throw new Error(`Expected value, got undefined. idx: ${idx}`);
 
-    if (value < num) {
-      min = average + 1;
-    } else {
-      max = average - 1;
-    }
+    value < num ? (min = idx + 1) : (max = idx - 1);
   }
 
   return -1;
