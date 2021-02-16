18b15894922249e90a87f1efe5d353b34b7ea614
Patryk Lizon <lizon.patryk@gmail.com>
2021-02-16 01:45:49 +0100

    refactor: Simplifu bubble sort

--- src/arrays/bubbleSort.ts
+++ src/arrays/bubbleSort.ts
@@ -1,14 +1,8 @@
 export const bubbleSort = (xs: number[]): number[] => {
   for (let i = 0; i < xs.length; i++) {
     for (let j = i + 1; j < xs.length; j++) {
-      // @ts-expect-error elements are numbers
-      if (xs[i] > xs[j]) {
-        const _i = xs[i];
-        // @ts-expect-error element is number
-        xs[i] = xs[j];
-        // @ts-expect-error element is number
-        xs[j] = _i;
-      }
+      // @ts-expect-error accessing elements that are numbers
+      if (xs[i] > xs[j]) [xs[i], xs[j]] = [xs[j], xs[i]];
     }
   }
 
