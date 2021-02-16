bd09860eca3d19cecb8a0be61638a25d2a313408
Patryk Lizoń <lizon.patryk@gmail.com>
2021-02-16 22:40:08 +0100

    style: Add steps description

--- src/arrays/bubble-sort.ts
+++ src/arrays/bubble-sort.ts
@@ -1,3 +1,8 @@
+// 1. Loop over each element of array.
+// 2. For each element (idx) loop over element remaining elements starting with next one (idx + 1).
+// 3. If element (idx) is higher than sequent element (idx + 1) swap them places.
+// 4. Continue looping and return the array.
+
 export const bubbleSort = (xs: number[]): number[] => {
   for (let i = 0; i < xs.length; i++) {
     let swapped = false;
