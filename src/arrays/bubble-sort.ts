5f56e7ae04cc44140e59fc93ab0cac6e41eb41dd
Patryk Lizon <lizon.patryk@gmail.com>
2021-02-16 09:51:11 +0100

    refactor: Optimize bubble sort

--- src/arrays/bubble-sort.ts
+++ src/arrays/bubble-sort.ts
@@ -1,9 +1,18 @@
 export const bubbleSort = (xs: number[]): number[] => {
   for (let i = 0; i < xs.length; i++) {
+    let swapped = false;
+
     for (let j = i + 1; j < xs.length; j++) {
       // @ts-expect-error accessing elements that are numbers
-      if (xs[i] > xs[j]) [xs[i], xs[j]] = [xs[j], xs[i]];
+      if (xs[i] > xs[j]) {
+        // @ts-expect-error accessing elements that are numbers
+        [xs[i], xs[j]] = [xs[j], xs[i]];
+        swapped = true;
+      }
     }
+
+    // array is already sorted
+    if (!swapped) break;
   }
 
   return xs;
