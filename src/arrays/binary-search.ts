c7895345e9487b25c4c1919d62e35aa765bd120b
Patryk Lizoń <lizon.patryk@gmail.com>
2021-02-16 22:39:53 +0100

    style: Update steps description

--- src/arrays/binary-search.ts
+++ src/arrays/binary-search.ts
@@ -1,9 +1,10 @@
-// 1. Let min = 0 and max = n.
-// 2. Guess the average of max and min, rounded down so that it is an integer.
-// 3. If you guessed the number, stop. You found it!
-// 4. If the guess was too low, set min to be one larger than the guess.
-// 5. If the guess was too high, set max to be one smaller than the guess.
+// 1. Let min = 0 and max = size of array.
+// 2. Calculate the average of max and min, rounded down so that it is an integer. It's our 'pointer' (idx) to array element.
+// 3. If pointed value is equal to searched number, stop. You found it!
+// 4. If pointed value is too low, set min to be one larger than the guess. (idx + 1).
+// 5. If pointed value is too high, set max to be one smaller than the guess. (idx - 1).
 // 6. Go back to step two.
+// 7. If element was not found return -1.
 
 export const binarySearch = (xs: number[], num: number): number => {
   let min = 0;
