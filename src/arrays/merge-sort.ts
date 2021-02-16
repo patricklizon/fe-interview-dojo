f9fb58bcf55040c6434ffa37e28eb963df43ab7f
Patryk Lizoń <lizon.patryk@gmail.com>
2021-02-16 22:56:00 +0100

    style: Add steps description

--- src/arrays/merge-sort.ts
+++ src/arrays/merge-sort.ts
@@ -1,3 +1,13 @@
+// 1. Find middle index.
+// 2. Slice array to 'left' and 'right'.
+// 3. Keep slicing both arrays until they will have 1 or 0 elements.
+// 4. Merge arrays:
+// 5. Let 'left pointer' (idx of left) and 'right pointer' (idx of right) = 0.
+// 6. If 'left value' is higher than 'right value' save it to new array and increment left index,
+//    otherwise do it for 'right value'.
+// 7. Repeat until 'left pointer' or 'right pointer' will be higher then size of its array.
+// 8. Concatenate 'result array' with slices of 'left array' and 'right array'.
+
 const merge = (left: number[], right: number[]): number[] => {
   const result = [];
   let lIdx = 0;
@@ -21,9 +31,9 @@ const merge = (left: number[], right: number[]): number[] => {
 export const mergeSort = (xs: number[]): number[] => {
   if (xs.length <= 1) return xs;
 
-  const middle = Math.floor(xs.length / 2);
-  const left = xs.slice(0, middle);
-  const right = xs.slice(middle);
+  const middleIdx = Math.floor(xs.length / 2);
+  const left = xs.slice(0, middleIdx);
+  const right = xs.slice(middleIdx);
 
   return merge(mergeSort(left), mergeSort(right));
 };
