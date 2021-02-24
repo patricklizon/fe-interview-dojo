23692dc671ebd555b2d0aa29fae7c1c735a9897d
Patryk Lizoń <lizon.patryk@gmail.com>
2021-02-24 19:58:11 +0100

    refactor: Reduce number of calls of factorial()

--- src/factorial.ts
+++ src/factorial.ts
@@ -1,3 +1,4 @@
 // Function could be wrapped with memoize for optimization
 
-export const factorial = (x: number): number => (x ? x * factorial(x - 1) : 1);
+export const factorial = (x: number): number =>
+  x < 2 ? 1 : x * factorial(x - 1);
--- tests/factorial.spec.ts
+++ tests/factorial.spec.ts
@@ -3,9 +3,23 @@ import { test } from "uvu";
 
 import { factorial } from "../src/factorial";
 
-test("computes correctly", () => {
+test("computes correctly for negative numbers", () => {
   const expected = 1;
-  const actual = factorial(0);
+  const actual = factorial(-4);
+
+  assert.is(actual, expected);
+});
+
+test("computes correctly for values lesser than 2", () => {
+  const data: number[] = [-1, 0, 1].map(factorial);
+  const expected = 1;
+
+  data.forEach((x) => assert.is(x, expected, `${x} is not equal ${expected}`));
+});
+
+test("computes correctly for 2", () => {
+  const expected = 2;
+  const actual = factorial(2);
 
   assert.is(actual, expected);
 });
